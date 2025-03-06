e8701cf88dcf4dc1ec08d0c80f586be34db643bd
thiago <tgtahim@gmail.com>
2025-03-06 16:41:23 -0300

    First Commit

--- src/main/java/com/dio/ProjetoDioSpringApplication.java
+++ src/main/java/com/dio/ProjetoDioSpringApplication.java
@@ -8,6 +8,7 @@ public class ProjetoDioSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoDioSpringApplication.class, args);
+		System.out.println("");
 	}
 
 }
