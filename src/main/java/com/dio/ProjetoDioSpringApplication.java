b60756d76dc0d839e91fb5eb4b2aee8e46816bbc
thiago <tgtahim@gmail.com>
2025-03-06 16:35:10 -0300

    First Commit

---
+++ src/main/java/com/dio/ProjetoDioSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDioSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDioSpringApplication.class, args);
+	}
+
+}
